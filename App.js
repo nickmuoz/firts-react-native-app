43ac8349a98d3f6d5b4a18939342ef5a6beea526
unknown <nicolasmuoz@gmail.com>
2023-03-29 23:24:17 -0500

    <commit message>

--- App.js
+++ App.js
@@ -1,11 +1,56 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Text, TextInput, TouchableOpacity, View, FlatList } from 'react-native';
 
 export default function App() {
+  const [task, setTask] = useState('');
+  const [taskList, setTaskList] = useState([]);
+
+  const handleAddTask = () => {
+    if (task.trim() === '') {
+      return;
+    }
+    const newTask = {
+      id: Math.random().toString(),
+      task: task,
+    };
+    setTaskList([...taskList, newTask]);
+    setTask('');
+  };
+
+  const handleDeleteTask = (id) => {
+    const filteredTaskList = taskList.filter((task) => task.id !== id);
+    setTaskList(filteredTaskList);
+  };
+
+  const renderItem = ({ item }) => (
+    <View style={styles.taskItem}>
+      <Text style={styles.taskText}>{item.task}</Text>
+      <TouchableOpacity style={styles.deleteButton} onPress={() => handleDeleteTask(item.id)}>
+        <Text style={styles.deleteButtonText}>Delete</Text>
+      </TouchableOpacity>
+    </View>
+  );
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.title}>Task Manager</Text>
+      <View style={styles.inputContainer}>
+        <TextInput
+          style={styles.input}
+          placeholder="Add a task..."
+          value={task}
+          onChangeText={(text) => setTask(text)}
+        />
+        <TouchableOpacity style={styles.addButton} onPress={handleAddTask}>
+          <Text style={styles.addButtonLabel}>Add</Text>
+        </TouchableOpacity>
+      </View>
+      <FlatList
+        data={taskList}
+        renderItem={renderItem}
+        keyExtractor={(item) => item.id}
+        style={styles.taskList}
+      />
     </View>
   );
 }
@@ -14,7 +59,61 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
+    padding: 16,
+  },
+  title: {
+    fontSize: 32,
+    fontWeight: 'bold',
+    textAlign: 'center',
+    marginBottom: 16,
+  },
+  inputContainer: {
+    flexDirection: 'row',
     alignItems: 'center',
-    justifyContent: 'center',
+    marginBottom: 16,
+  },
+  input: {
+    flex: 1,
+    height: 40,
+    borderWidth: 1,
+    borderRadius: 8,
+    marginRight: 16,
+    padding: 8,
+    fontSize: 16,
+  },
+  addButton: {
+    backgroundColor: '#008080',
+    borderRadius: 8,
+    padding: 8,
+  },
+  addButtonLabel: {
+    color: '#fff',
+    fontSize: 16,
+    fontWeight: 'bold',
+  },
+  taskList: {
+    flex: 1,
+  },
+  taskItem: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    backgroundColor: '#f0f0f0',
+    borderRadius: 8,
+    padding: 16,
+    marginBottom: 8,
+  },
+  taskText: {
+    fontSize: 16,
+  },
+  deleteButton: {
+    backgroundColor: '#ff0000',
+    borderRadius: 8,
+    padding: 8,
+  },
+  deleteButtonText: {
+    color: '#fff',
+    fontSize: 16,
+    fontWeight: 'bold',
   },
 });
